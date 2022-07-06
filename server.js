e2d3124eac5bf2183665199c07e98b930355e183
Gulshan <gulshan190210@gmail.com>
2022-07-06 23:25:04 +0530

    Home page created

--- server.js
+++ server.js
@@ -1,15 +1,23 @@
 const express = require('express');
+const ejs = require("ejs");
+const path = require("path");
+const expressEjsLayouts = require("express-ejs-layouts");
 require("dotenv").config();
 
 const app = express();
 
+
+
+
+// app.use(expressEjsLayouts);
+app.set("views", path.join(__dirname,"/resources/views"));
 app.set('view engine', 'ejs');
 app.use(express.static('public'))
 
 app.get("/", (req,res)=>
 {
     res.render("home");
-})
+});
 
 app.listen(process.env.PORT, (err)=>
 {
